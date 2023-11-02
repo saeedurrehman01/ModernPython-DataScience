693207a939ff688c6286f6a580c96649ff34ed1e
saeedurrehman01 <139468834+saeedurrehman01@users.noreply.github.com>
2023-11-02 15:11:33 +0500

    Class 9 Completed

---
+++ 09_class(loops_input)/class9.py
@@ -0,0 +1,11 @@
+import sys 
+  
+  
+print("This is the name of the program:", 
+       sys.argv[0]) 
+print("Number of elements including the name of the program:", 
+       len(sys.argv)) 
+print("Number of elements excluding the name of the program:", 
+      (len(sys.argv)-1)) 
+print("Argument List:", 
+       str(sys.argv[1:]))
